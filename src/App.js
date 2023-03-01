202a988cff741589c85f34ee06dfef3432e001ec
Prakhar <prakhar.tripathi1@learner.manipal.edu>
2023-03-02 01:23:46 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class MapContainer extends Component {
+  render () {
+  return(
+        <Map
+        google={this.props.google}
+        style={{width:"100%", height:"100%"}}
+        zoom={15}
+        initialCenter ={
+          {
+            lat:13.3410,
+            lng:74.7421,
+          }
+        }
+        />
   );
 }
+}
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: ("AIzaSyCgct0MqU-1kR2Pt4kIsaLcNuqC2iLzcdw")
+})(MapContainer)
